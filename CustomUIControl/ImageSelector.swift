d9b06184abdcb40f5155dfa923e620b78705c5af
Tianbo Qiu <tianboqiu@TMBP0.attlocal.net>
2022-12-28 15:17:34 -0800

    highlight view

--- CustomUIControl/ImageSelector.swift
+++ CustomUIControl/ImageSelector.swift
@@ -11,7 +11,19 @@ import UIKit
 class ImageSelector: UIControl {
     
     // currently selected image
-    var selectedIndex = 0
+    var selectedIndex = 0 {
+        didSet {
+            if selectedIndex < 0 {
+                selectedIndex = 0
+            }
+            if selectedIndex >= imageButtons.count {
+                selectedIndex = imageButtons.count - 1
+            }
+            
+            let imageButton = imageButtons[selectedIndex]
+            highlightViewXConstraint = highlightView.centerXAnchor.constraint(equalTo: imageButton.centerXAnchor)
+        }
+    }
     
     // a list of images to select
     var images: [UIImage] = [] {
@@ -58,12 +70,16 @@ class ImageSelector: UIControl {
     
     private func configureViewHierarchy() {
         addSubview(selectorStackView)
+        insertSubview(highlightView, belowSubview: selectorStackView)
         
         NSLayoutConstraint.activate([
             selectorStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
             selectorStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
             selectorStackView.topAnchor.constraint(equalTo: topAnchor),
             selectorStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            highlightView.heightAnchor.constraint(equalTo: highlightView.widthAnchor),
+            highlightView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.9),
+            highlightView.centerYAnchor.constraint(equalTo: selectorStackView.centerYAnchor),
         ])
     }
     
@@ -75,5 +91,25 @@ class ImageSelector: UIControl {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         configureViewHierarchy()
+    }
+    
+    private let highlightView: UIView = {
+       let view = UIView()
+        view.backgroundColor = view.tintColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private var highlightViewXConstraint: NSLayoutConstraint! {
+        didSet {
+            oldValue?.isActive = false
+            highlightViewXConstraint.isActive = true
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        highlightView.layer.cornerRadius = highlightView.bounds.height / 2.0
     }
 }
